59584fdaade856b79b4fc424fb161d6ed41e0cb2
Karl <karlding@users.noreply.github.com>
2019-03-18 15:13:59 -0700

    Add Travis CI linter and docs checks
    Add build steps for building Sphinx documentation and running linter
    checks

--- setup.py
+++ setup.py
@@ -22,7 +22,10 @@ setup(
     packages=find_packages(exclude=["tests"]),
     install_requires=["pyparsing>=2.3.1", "Jinja2>=2.10.1"],
     # pip install -e .[docs]
-    extras_require={"docs": ["m2r>=0.2.0", "Sphinx>=2.0.0", "sphinx-rtd-theme>=0.4.0"]},
+    extras_require={
+        "docs": ["m2r>=0.2.0", "Sphinx>=2.0.0", "sphinx-rtd-theme>=0.4.0"],
+        "dev": ["black==19.3b0"],
+    },
     test_suite="tests",
     entry_points={"console_scripts": ["canlogconvert=canlogconvert.__init__:_main"]},
 )
